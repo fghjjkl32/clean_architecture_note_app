b2c64f22f191fd9ee1dfe6ba276e702866ae278e
Geon Woo Jeon <fghjjkl2700@gmail.com>
2022-11-11 20:25:56 +0900

    add : note CRUD use case code

---
+++ lib/domain/use_case/get_notes.dart
@@ -0,0 +1,13 @@
+import 'package:clean_architecture_note_app/domain/model/note.dart';
+import 'package:clean_architecture_note_app/domain/repository/note_repository.dart';
+
+class GetNotes {
+  final NoteRepository repository;
+
+  GetNotes(this.repository);
+
+  Future<List<Note>> call() async {
+    List<Note> notes = await repository.getNotes();
+    return notes;
+  }
+}
